7600b7e19e71a0891f509ea3f586725efb9bfe17
Jens Fischer <jens@weareprofound.com>
2024-02-23 17:58:30 -0500

    Initial Config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Jens Fischer</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/artofrawr',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/artofrawr/garden/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    component: '',
   },
 }
 
